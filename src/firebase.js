50d15ac5a56062322e3043b6cb69d22eaf6eb99d
adman94 <adman00794@gmail.com>
2020-09-01 05:00:32 -0700

    header component added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3dze8npYhpA6NDx2ulaJwK_QBp7uqX_o",
+  authDomain: "facebook-clone-9f062.firebaseapp.com",
+  databaseURL: "https://facebook-clone-9f062.firebaseio.com",
+  projectId: "facebook-clone-9f062",
+  storageBucket: "facebook-clone-9f062.appspot.com",
+  messagingSenderId: "599317580698",
+  appId: "1:599317580698:web:8261b4bdd84e7e910f8823",
+  measurementId: "G-5ELRJYFQDS"
+};
